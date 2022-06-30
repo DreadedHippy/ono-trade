cea2d3ba9932209b9cbb13441322e22f626e597b
DreadedHippy <onotaizee@gmail.com>
2022-06-30 09:38:36 +0100

    App name changed

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'light',
+  appName: 'OnoTrade',
   webDir: 'www',
   bundledWebRuntime: false,
   cordova: {
