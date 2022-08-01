4b0cba154df4c396320a01213ae813fc7c96568d
DreadedHippy <onotaizee@gmail.com>
2022-08-02 00:38:36 +0100

    P2P page design done

--- src/app/pages/market/peer/peer.page.ts
+++ src/app/pages/market/peer/peer.page.ts
@@ -7,9 +7,94 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PeerPage implements OnInit {
 
+  offers = [
+    {
+      picSrc: 'www.google.com',
+      name: 'User 1',
+      ratings: 4.0, //0.0 - 5.0, 5-star scale
+      tradesCount: 900,
+      percentageCompleted: 90, //Percentage of trades completed
+      type: 'sell', //What type of offer is made by the trader
+      price: 1, //Asking price of the trader in bank currency
+      acceptedCurr: 'usd', //Bank Currency accepted by the Peer offering the crypto
+      inStock: 14000, //Amount of crypto left in stock
+      cryptoCurr: 'usdc', //The Cryptocurrency being offered
+      upperLimit: 10000, //Maximum amount of crypto that can be bought at a time
+      lowerLimit: 5000, //Minimum amount of crypto that can be bought a time.
+      paymentMethods: [
+        'zel', 'ppl', 'pst'
+      ]
+    },
+    {
+      picSrc: 'www.google.com/newman',
+      name: 'Ezreal',
+      ratings: 4.9, //0.0 - 5.0, 5-star scale
+      tradesCount: 728,
+      price: 1, //Asking price of the trader in bank currency
+      percentageCompleted: 95, //Percentage of trades completed
+      type: 'buy', //what type of offer is made by the trader
+      acceptedCurr: 'ngn', //Bank Currency accepted by the Peer offering the crypto
+      inStock: 300000, //Amount of crypto left in stock
+      cryptoCurr: 'eth', //The Cryptocurrency being offered
+      upperLimit: 100000, //Maximum amount of crypto that can be bought at a time
+      lowerLimit: 80000, //Minimum amount of crypto that can be bought a time.
+      paymentMethods:[
+        'zel', 'ppl'
+      ]
+    },
+
+    {
+      picSrc: 'www.google.com',
+      name: 'User 1',
+      ratings: 2.7, //0.0 - 5.0, 5-star scale
+      tradesCount: 900,
+      percentageCompleted: 90, //Percentage of trades completed
+      type: 'sell', //what type of offer is made by the trader
+      price: 1, //Asking price of the trader in bank currency per unit of crypto
+      acceptedCurr: 'usd', //Bank Currency accepted by the Peer offering the crypto
+      inStock: 14000, //Amount of crypto left in stock
+      cryptoCurr: 'btc', //The Cryptocurrency being offered
+      upperLimit: 10000, //Maximum amount of crypto that can be bought at a time
+      lowerLimit: 5000, //Minimum amount of crypto that can be bought a time.
+      paymentMethods: [
+        'ppl', 'pst'
+      ]
+    }
+  ]
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ratingsStar(rating: number){
+    if(rating >= 4.9){
+      return ['star','star','star','star','star']
+    }
+    if(rating >= 4.5){
+      return['star','star','star','star','star-half']
+    }
+    if(rating >= 4.0){
+      return ['star','star','star','star','star-outline']
+    }
+    if(rating >= 3.5){
+      return ['star','star','star','star-half','star-outline']
+    }
+    if(rating >= 3.0){
+      return ['star','star','star','star-outline','star-outline']
+    }
+    if(rating >= 2.5){
+      return ['star','star','star-half','star-outline','star-outline']
+    }
+    if(rating >= 2.0){
+      return ['star','star','star-outline','star-outline','star-outline']
+    }
+    if(rating >= 1.5){
+      return ['star','star-half','star-outline','star-outline','star-outline']
+    }
+    if(rating >= 1.0){
+      return ['star','star-outline','star-outline','star-outline','star-outline']
+    }
   }
 
 }
