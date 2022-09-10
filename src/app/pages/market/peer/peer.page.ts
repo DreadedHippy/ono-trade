927300a7de604ce043cb66f299a49e2277c38131
DreadedHippy <onotaizee@gmail.com>
2022-09-10 02:03:55 +0100

    Peer trades page redesign

--- src/app/pages/market/peer/peer.page.ts
+++ src/app/pages/market/peer/peer.page.ts
@@ -13,58 +13,6 @@ export class PeerPage implements OnInit, OnDestroy {
 
   peerOffers: peerOffer[] = []
   offers: peerOffer[] = [
-    {
-      name: 'User 1',
-      ratings: 4.0, //0.0 - 5.0, 5-star scale
-      tradesCount: 900,
-      timeLimit: 15, //Time limit in minutes
-      percentageCompleted: 90, //Percentage of trades completed
-      type: 'sell', //What type of offer is made by the trader
-      price: 1, //Asking price of the trader in bank currency
-      fiatCurr: 'usd', //Bank Currency accepted by the Peer offering the crypto
-      inStock: 14000, //Amount of crypto left in stock
-      cryptoCurr: 'usdc', //The Cryptocurrency being offered
-      upperLimit: 10000, //Maximum amount of crypto that can be bought at a time
-      lowerLimit: 5000, //Minimum amount of crypto that can be bought a time.
-      paymentMethods: [
-        'zel', 'ppl', 'pst'
-      ]
-    },
-    {
-      name: 'Ezreal',
-      ratings: 4.9, //0.0 - 5.0, 5-star scale
-      tradesCount: 728,
-      timeLimit: 15, //Time limit in minutes
-      price: 1, //Asking price of the trader in bank currency
-      percentageCompleted: 95, //Percentage of trades completed
-      type: 'buy', //what type of offer is made by the trader
-      fiatCurr: 'ngn', //Bank Currency accepted by the Peer offering the crypto
-      inStock: 300000, //Amount of crypto left in stock
-      cryptoCurr: 'eth', //The Cryptocurrency being offered
-      upperLimit: 100000, //Maximum amount of crypto that can be bought at a time
-      lowerLimit: 80000, //Minimum amount of crypto that can be bought a time.
-      paymentMethods:[
-        'zel', 'ppl'
-      ]
-    },
-
-    {
-      name: 'User 1',
-      ratings: 2.7, //0.0 - 5.0, 5-star scale
-      tradesCount: 900, //Number of trades,
-      timeLimit: 15, //Time limit in minutes
-      percentageCompleted: 90, //Percentage of trades completed
-      type: 'sell', //what type of offer is made by the trader
-      price: 1, //Asking price of the trader in bank currency per unit of crypto
-      fiatCurr: 'usd', //Bank Currency accepted by the Peer offering the crypto
-      inStock: 14000, //Amount of crypto left in stock
-      cryptoCurr: 'btc', //The Cryptocurrency being offered
-      upperLimit: 10000, //Maximum amount of crypto that can be bought at a time
-      lowerLimit: 5000, //Minimum amount of crypto that can be bought a time.
-      paymentMethods: [
-        'ppl', 'pst'
-      ]
-    }
   ]
   subs = new SubSink()
 
